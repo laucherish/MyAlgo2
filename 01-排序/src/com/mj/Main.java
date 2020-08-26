4a0b09e99873f93d7c3a8d3a79d0a7a945a343c4
laucherish <laucherish@163.com>
2020-08-26 14:12:11 +0800

    Add sort.

--- 01-排序/src/com/mj/Main.java
+++ 01-排序/src/com/mj/Main.java
@@ -1,26 +1,77 @@
 package com.mj;
 
+import com.mj.sort.*;
+import com.mj.tools.Asserts;
 import com.mj.tools.Integers;
 import com.mj.tools.Times;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Integer[] array1 = Integers.tailAscOrder(1, 10000, 2000);
-        Integer[] array2 = Integers.copy(array1);
-        Integer[] array3 = Integers.copy(array1);
+//        Integer[] array1 = Integers.tailAscOrder(1, 10000, 2000);
+//        Integer[] array2 = Integers.copy(array1);
+//        Integer[] array3 = Integers.copy(array1);
+//
+//        Times.test("冒泡排序", () -> {
+//            bubbleSort1(array1);
+//        });
+//
+//        Times.test("冒泡排序2", () -> {
+//            bubbleSort2(array2);
+//        });
+//
+//        Times.test("冒泡排序3", () -> {
+//            bubbleSort3(array3);
+//        });
 
-        Times.test("冒泡排序", () -> {
-            bubbleSort1(array1);
-        });
+        Integer[] array = Integers.random(10000, 1, 20000);
+//        Integer[] array2 = Integers.copy(array1);
+//        Integer[] array3 = Integers.copy(array1);
 
-        Times.test("冒泡排序2", () -> {
-            bubbleSort2(array2);
-        });
+//        Times.test("HeapSort",()->{
+//            new HeapSort().sort(array1);
+//        });
+//        Times.test("SelectionSort",()->{
+//            new SelectionSort().sort(array2);
+//        });
+//        Times.test("BubbleSort3",()->{
+//            new BubbleSort3().sort(array3);
+//        });
 
-        Times.test("冒泡排序3", () -> {
-            bubbleSort3(array3);
-        });
+        testSort(array,
+               new BubbleSort1(),
+               new BubbleSort2(),
+               new HeapSort(),
+               new SelectionSort(),
+               new BubbleSort3()
+        );
+    }
 
+    static void testSort(Integer[] array, Sort... sorts) {
+        for (Sort sort : sorts) {
+            sort.sort(Integers.copy(array));
+        }
+
+        Arrays.sort(sorts);
+
+        for (Sort sort : sorts) {
+            System.out.println(sort);
+        }
+    }
+
+    static void selectionSort(Integer[] array) {
+        for (int end = array.length - 1; end > 0; end--) {
+            int maxIndex = 0;
+            for (int begin = 1; begin <= end; begin++) {
+                if (array[maxIndex] <= array[begin]) {
+                    maxIndex = begin;
+                }
+            }
+            int tmp = array[maxIndex];
+            array[maxIndex] = array[end];
+            array[end] = tmp;
+        }
     }
 
     static void bubbleSort1(Integer[] array) {
---
+++ 01-排序/src/com/mj/sort/BubbleSort3.java
@@ -0,0 +1,18 @@
+package com.mj.sort;
+
+public class BubbleSort3 extends Sort {
+    @Override
+    protected void sort() {
+        for (int end = array.length - 1; end > 0; end--) {
+            // sortedIndex的初始值在数组完全有序的时候有用
+            int sortedIndex = 0;
+            for (int begin = 1; begin <= end; begin++) {
+                if (cmp(begin, begin - 1) < 0) {
+                    swap(begin, begin - 1);
+                    sortedIndex = begin;
+                }
+            }
+            end = sortedIndex;
+        }
+    }
+}
